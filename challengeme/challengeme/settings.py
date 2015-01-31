39509b3ff26330471b44308674413d2d9ed9fd78
Max Fang <maxfangx@gmail.com>
2015-01-31 02:27:52 -0800

    Add PostgreSQL

--- challengeme/challengeme/settings.py
+++ challengeme/challengeme/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'challengeme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'basedb',
+	'USER': 'basedadmin',
+	'PASSWORD': 'live',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
