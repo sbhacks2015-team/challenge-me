d73270c0e854e4123cfdeb4298ab515cea4ab03c
Max Fang <maxfangx@gmail.com>
2015-01-31 15:39:53 -0800

    Challenges

--- challengeme/base/urls.py
+++ challengeme/base/urls.py
@@ -3,6 +3,7 @@ import challengeme.base.views as views
 
 urlpatterns = patterns('',
     url(r'^$', views.poop, name='poop'),
-    url(r'^challenge$', views.AllChallengesView.as_view(), name='challenge'),
-    # url(r'^challenge/
+    url(r'^challenges$', views.AllChallengesView.as_view(), name='challenges'),
+    # url(r'^challenges/id$', views.AllInstancesView.as_view(), name='instances'),
+
 )
