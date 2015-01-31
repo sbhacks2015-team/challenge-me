4452a874ca283957bbeaa966c614a5a1d6bf59f8
Max Fang <maxfangx@gmail.com>
2015-01-31 10:10:36 -0800

    Add defaults and requirements

--- base/models.py
+++ base/models.py
@@ -2,27 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Charity(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(blank=False, max_length=256)
+    email = models.CharField(max_length=256)
     description = models.CharField(max_length=60000)
     bitcoin_address = models.CharField(min_length=26, max_length=35)
 
 
 class Challenge(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(blank=False, max_length=256)
     description = models.CharField(max_length=60000)
     owner = models.ForeignKey(User)
     charity = models.ForeignKey(Charity)
 
 
 class Instance(models.Model):
-    title = models.CharField(max_length=256)
+    challenge = models.ForeignKey(Challenge)
+    default_title = challenge.title
+    title = models.CharField(blank=False, default=default_title, max_length=256)
     note = models.CharField(max_length=60000)
     release_date = models.DateField(auto_now_add=True)
     goal_date = models.DateField()
     bounty = models.DecimalField(max_digits=14, decimal_places=8)
 
     owner = models.ForeignKey(User)
-    challenge = models.ForeignKey(Challenge)
     participants = models.ManyToManyField(User, related_name="participants")
     supporters = models.ManyToManyField(User, related_name="supporters")
 
