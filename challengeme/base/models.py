069cda6758be52f202659b06e13280ee82c4def1
Max Fang <maxfangx@gmail.com>
2015-01-31 01:18:42 -0800

    Add more variables and defaults

--- challengeme/base/models.py
+++ challengeme/base/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.contrib.auth.models import Challenge, Instance, User, Charity
 
 class Challenge(models.Model):
+	context_id = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=256)
+	description = models.CharField(max_length=60000)
+
 	owner = models.ForeignKey(User)
 	charity = models.ForeignKey(Charity)
 
 
 class Instance(models.Model):
+	context_id = models.IntegerField(primary_key=True)
+	note = models.CharField(max_length=60000)
+	release_date = models.DateField(auto_now_add=True)
+	goal_date = models.DateField()
+	bounty = models.DecimalField(max_digits=4, decimal_places=8)
+
 	owner = models.ForeignKey(User)
 	challenge = models.ForeignKey(Challenge)
 	participants = models.ManyToMany(User)
@@ -14,10 +24,12 @@ class Instance(models.Model):
 
 
 class User(models.Model):
+	context_id = models.IntegerField(primary_key=True)
 	username = models.CharField(max_length=100)
 	email = models.CharField(max_length=256)
 
 
 class Charity(models.Model):
+	context_id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=256)
-	description = models.CharField()
+	description = models.CharField(max_length=60000)
