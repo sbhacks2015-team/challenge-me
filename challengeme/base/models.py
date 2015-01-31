67b8665be36acc00f5af67b95741bf6789400b6a
Max Fang <maxfangx@gmail.com>
2015-01-31 00:59:13 -0800

    All relationships simplified and done

--- challengeme/base/models.py
+++ challengeme/base/models.py
@@ -1,32 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import Challenge, User, Charity, Instance, Owner, Participant, Supporter
+from django.contrib.auth.models import Challenge, Instance, User, Charity
 
 class Challenge(models.Model):
 	owner = models.ForeignKey(User)
+	charity = models.ForeignKey(Charity)
 
-	
+
+class Instance(models.Model):
+	owner = models.ForeignKey(User)
+	challenge = models.ForeignKey(Challenge)
+	participants = models.ManyToMany(User)
+	supporters = models.ManyToMany(User)
+
+
 class User(models.Model):
 	username = models.CharField(max_length=100)
 	email = models.CharField(max_length=256)
 
 
 class Charity(models.Model):
-	pass
-
-
-class Instance(models.Model):
-	pass
-
-
-class Owner(models.Model):
-	pass
-
-
-class Participant(models.Model):
-	pass
-
-
-class Supporter(models.Model):
-	pass
-
-	
+	name = models.CharField(max_length=256)
+	description = models.CharField()
