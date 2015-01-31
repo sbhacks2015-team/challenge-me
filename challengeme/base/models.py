7fc01e5ffe57c3806dbbf822542b2ba500ba8105
Max Fang <maxfangx@gmail.com>
2015-01-30 23:32:02 -0800

    Add charity

--- challengeme/base/models.py
+++ challengeme/base/models.py
@@ -1,21 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import Challenge, Instance, User, Owner, Participant, Supporter
+from django.contrib.auth.models import Challenge, User, Instance, Owner, Participant, Supporter
 
 class Challenge(models.Model):
 	owner = models.ForeignKey(User)
 
-class Instance(models.Model):
-	pass
 	
 class User(models.Model):
 	username = models.CharField(max_length=100)
 	email = models.CharField(max_length=256)
 
+
+class Charity(models.Model):
+	pass
+	
+
+class Instance(models.Model):
+	pass
+
+
 class Owner(models.Model):
 	pass
 
+
 class Participant(models.Model):
 	pass
+
 
 class Supporter(models.Model):
 	pass
