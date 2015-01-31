bfa08e5d6bd0414a77f3c8832a1d950d473f4657
Max Fang <maxfangx@gmail.com>
2015-01-30 23:01:47 -0800

    Starting models

--- challengeme/base/models.py
+++ challengeme/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import Challenge, Instance, User, Owner, Participant, Supporter
 
 class Challenge(models.Model):
 	owner = models.ForeignKey(User)
