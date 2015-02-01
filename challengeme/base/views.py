53f039190e002883a98d643dd2fde562b09b2ad9
Max Fang <maxfangx@gmail.com>
2015-01-31 23:19:10 -0800

    Start of dashboard

--- challengeme/base/views.py
+++ challengeme/base/views.py
@@ -4,27 +4,30 @@ from django.http import HttpResponse
 import datetime
 from django.template.response import TemplateResponse
 
-from challengeme.base.models import Challenge, Instance, User, Charity
+from challengeme.base.models import Challenge, Instance, User, Charity, Profile
 from django.contrib.auth.decorators import login_required
+
 
 class LandingPage(TemplateView):
     template_name = "home.html"
+
 
 def poop(request):
     now = datetime.datetime.now()
     html = "<html>sup bae it's %s</html>" % now
     return HttpResponse(html)
 
+
 class UserDashboard(ListView):
+    model = Profile
     template_name = "dashboard.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Dashboard, self).get_context_data(**kwargs)
+        context = super(UserDashboard, self).get_context_data(**kwargs)
         # Get context objects.
         context['own_challenges'] = Instance.objects.filter(owner=self.request.user)
         context['challenges_in'] = self.request.user.participants.all
         context['challenges_supporting'] = self.request.user.supporters.all
-
         return context
     
 class AllChallengesView(ListView):
@@ -36,15 +39,17 @@ class AllChallengesView(ListView):
         context['public_challenges'] = Challenge.objects.all 
         return context
 
+
 class ChallengeDetailView(DetailView):
     model = Challenge
     template_name = "challenge_detail.html"    
+
     def get_context_data(self, **kwargs):
         context = super(ChallengeDetailView, self).get_context_data(**kwargs)
         return context
 
+
 class InstanceDetailView(DetailView):
-    # called from url pattern r'^go/<id>/ or some shiet like that 
     model = Instance
     template_name = "instance_detail.html"
 
