0b3a823bfa5757636d1013d1aebf01edf01faf14
emesde <nougatcrunch@gmail.com>
2015-01-31 17:56:09 -0800

    poop

--- challengeme/base/urls.py
+++ challengeme/base/urls.py
@@ -3,10 +3,8 @@ import challengeme.base.views as views
 
 urlpatterns = patterns('',
     url(r'^$', views.poop, name='poop'),
-<<<<<<< HEAD
     url(r'^challenges$', login_required(views.AllChallengesView.as_view()), name='challenges'),
     # url(r'^challenges/id$', views.AllInstancesView.as_view(), name='instances'),
-=======
     url(r'^challenges$', 
         login_required(views.AllChallengesView.as_view()),
         name='challenges')
@@ -19,6 +17,4 @@ urlpatterns = patterns('',
     url(r'^go/(?P<instance_id>\d+)/$',
         login_required(views.InstanceView.as_view()),
         name='instance'),
->>>>>>> 051a60afb26231186b3ba8feee27e22c409cfcb4
-
 )
