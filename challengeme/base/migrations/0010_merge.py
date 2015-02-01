3b02099eb571dd6df969105b8073b250e615fa98
emesde <nougatcrunch@gmail.com>
2015-02-01 08:47:19 -0800

    dashboard fixed

---
+++ challengeme/base/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0009_auto_20150201_1339'),
+        ('base', '0009_auto_20150201_1430'),
+    ]
+
+    operations = [
+    ]
